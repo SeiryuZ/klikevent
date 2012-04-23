d5c6ffbf7fff106940932667df9d8f8bd570cd82
steven <steven@steven-VirtualBox.(none)>
2012-04-23 19:02:05 +0700

    added route of existing klikevent and created default template action for it

--- src/KlikEvent/EventBundle/Controller/DefaultController.php
+++ src/KlikEvent/EventBundle/Controller/DefaultController.php
@@ -12,4 +12,9 @@ class DefaultController extends Controller
     {
         return $this->render('KlikEventEventBundle:Default:index.html.twig', array('name' => $name));
     }
+
+    public function templateAction($name = "default")
+    {
+        return $this->render('KlikEventEventBundle:Default:template.html.twig', array('name' => $name));
+    }
 }
