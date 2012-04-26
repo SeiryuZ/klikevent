20c024240da14f17811752600cdf64de070b5cd4
steven <steven@steven-VirtualBox.(none)>
2012-04-26 09:58:42 +0700

    Created two twig for hot event and showing event by date, Changed the pattern for month to be using integer only

--- src/KlikEvent/EventBundle/Controller/DefaultController.php
+++ src/KlikEvent/EventBundle/Controller/DefaultController.php
@@ -5,7 +5,11 @@ namespace KlikEvent\EventBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Doctrine\ORM\Query\ResultSetMapping;
+
 use KlikEvent\EventBundle\Entity\Subscriber;
+use KlikEvent\EventBundle\Entity\Event;
+use KlikEvent\EventBundle\Entity\Category;
 
 use KlikEvent\EventBundle\Form\SubscribeType;
 
@@ -21,6 +25,64 @@ class DefaultController extends Controller
     {
         return $this->render('KlikEventEventBundle:Default:template.html.twig', array('name' => $name));
     }
+
+/***HOT START***/
+    public function showHotAction( Request $request )
+    {
+        $repository = $this->getDoctrine()->getRepository('KlikEventEventBundle:Event');
+        
+        $events = $repository->findBy(
+            array('isHot' => '1'),
+            array('viewCount' => 'DESC')
+        );
+
+    return $this->render('KlikEventEventBundle:Default:hot_event.html.twig', array('events' => $events, 'pageTitle'=>'Hot Events'));
+    }
+/***HOT START***/
+
+
+/***Today START***/
+
+//TODO Optimization of getting next and previous dates
+//TODO checking if given date and month is incorrect
+    public function showEventByDateAction( Request $request,  $date = 0 , $month = 0 , $year = 0 )
+    {
+
+
+
+        $today = date ('Y-m-d ');
+
+
+        if ( $date != 0 && $month != 0 && $year != 0 )
+            $today = date( 'Y-m-d', strtotime( $year."-".$month."-".$date ) );
+
+        //$next = $this->addIntervalToDate( $today ,"Y-m-d", 1);
+        //$prev = $this->addIntervalToDate( $today ,"Y-m-d", -1);
+
+        $repository = $this->getDoctrine()->getRepository('KlikEventEventBundle:Event');
+        $qb = $repository->createQueryBuilder('e');
+
+        $qb
+            ->innerJoin('e.eventTimes' , 't', 'WITH', 'e.id =  t.event' )
+            ->where($qb->expr()->like('t.begin' , '?1' ))
+            ->orderBy('e.viewCount')
+            ->setParameter( 1, $today."%" );
+        $events = $qb->getQuery()->getResult();
+
+        return $this->render('KlikEventEventBundle:Default:list_event.html.twig', 
+            array(
+                'events' => $events, 
+                'pageTitle'=>'Events ', 
+                'date'=> $today,
+                'nextDate'=> $this->addIntervalToDate( $today ,"d", 1),
+                'nextMonth'=> $this->addIntervalToDate( $today ,"m", 1),
+                'nextYear'=> $this->addIntervalToDate( $today ,"Y", 1),
+                'prevDate'=>$this->addIntervalToDate( $today ,"d", -1),
+                'prevMonth'=>$this->addIntervalToDate( $today ,"m", -1),
+                'prevYear'=>$this->addIntervalToDate( $today ,"Y", -1)
+                ));
+    }
+/***Today START***/
 
 
 /***SUBSCRIBE START***/
@@ -60,4 +122,15 @@ class DefaultController extends Controller
     }
 
 /***SUBSCRIBE END***/
+
+
+
+    private function addIntervalToDate ($d ="", $format="Y-m-d", $interval )
+    {
+        if ($d =="") 
+            $d=date("Y-m-d");
+        return date($format, mktime(0,0,0,date('m',strtotime($d)), date('d',strtotime($d))+$interval, date('Y',strtotime($d)) ));
+    }
+
+
 }
